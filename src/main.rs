d8174bb5234924028d6abb0b2c1e5affff6bd7d2
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2019-09-18 23:16:21 +0200

    rayon test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use raster;
+use rayon;
 
 fn threshhold(image: &mut raster::Image, threshold: u8) {
     image.bytes.iter_mut().for_each(|b| {
@@ -31,36 +32,95 @@ fn discard_block(image: &mut raster::Image, x: i32, y: i32, width: i32, height: 
     }
 }
 
+struct Chunk {
+    start: *mut usize,
+    width: usize,
+    height: usize,
+}
+
+impl Chunk {
+    fn check(&self) {
+        let mut blacks = 0;
+        for i in x..x + width {
+            for j in y..y + height {
+                if image.get_pixel(i, j).unwrap().a == 0 {
+                    blacks += 1;
+                };
+            }
+        }
+        blacks as f32 > (width * height) as f32 * (fill_percent as f32 / 100 as f32)
+    }
+
+    fn get_pixel_color(x: i32, y: i32) -> u8 {
+        let rgba = 4;
+        let start = (y * self.width) + x;
+        let start = start * rgba;
+
+            let slice = &self.bytes[start as usize..end as usize];
+            Ok(Color {
+                r: slice[0],
+                g: slice[1],
+                b: slice[2],
+                a: slice[3],
+            })
+        }
+    }
+}
+
 fn discard_blocks(image: &mut raster::Image, block_size: i32, fill_percent: u8) {
     let width = image.width;
     let height = image.height;
 
     let mut i = 0;
     let mut j;
+    let splits = height / block_size;
     loop {
         j = 0;
-        loop {
-            if check_block(
-                image,
-                i,
-                j,
-                std::cmp::min(block_size, width - i),
-                std::cmp::min(block_size, height - j),
-                fill_percent,
-            ) {
-                discard_block(
-                    image,
-                    i,
-                    j,
-                    std::cmp::min(block_size, width - i),
-                    std::cmp::min(block_size, height - j),
-                );
+        rayon::scope(|s| {
+            for j in 0..splits + 1 {
+                let y = j * block_size;
+                s.spawn(|_| {
+                    if check_block(
+                        image,
+                        i,
+                        y,
+                        std::cmp::min(block_size, width - i),
+                        std::cmp::min(block_size, height - y),
+                        fill_percent,
+                    ) {
+                        discard_block(
+                            image,
+                            i,
+                            j,
+                            std::cmp::min(block_size, width - i),
+                            std::cmp::min(block_size, height - y),
+                        );
+                    }
+                });
             }
-            j += block_size;
-            if j > height {
-                break;
-            }
-        }
+        });
+        //        loop {
+        //            if check_block(
+        //                image,
+        //                i,
+        //                j,
+        //                std::cmp::min(block_size, width - i),
+        //                std::cmp::min(block_size, height - j),
+        //                fill_percent,
+        //            ) {
+        //                discard_block(
+        //                    image,
+        //                    i,
+        //                    j,
+        //                    std::cmp::min(block_size, width - i),
+        //                    std::cmp::min(block_size, height - j),
+        //                );
+        //            }
+        //            j += block_size;
+        //            if j > height {
+        //                break;
+        //            }
+        //        }
         i += block_size;
         if i > width {
             break;
