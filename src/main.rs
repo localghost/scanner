d96f16e251db82d38197ab548ff0d0c3e631c199
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2019-09-20 06:43:09 +0200

    Handle CLI options.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use raster;
+use std::io::{BufRead, Write};
 use structopt::StructOpt;
 
 mod scanner;
@@ -8,6 +9,9 @@ mod scanner;
 struct Options {
     #[structopt(short, long, default_value = "120")]
     threshold: u8,
+
+    #[structopt(short = "d", long)]
+    block_discard_disabled: bool,
 
     #[structopt(short, long, default_value = "50")]
     block_size: u8,
@@ -19,7 +23,15 @@ struct Options {
     output: std::path::PathBuf,
 }
 
-fn main() {
+fn handle_file(input: &std::path::Path, output: &std::path::Path, options: &Options) {
+    let mut image = raster::open(input.to_str().unwrap()).unwrap();
+    raster::filter::grayscale(&mut image).unwrap();
+    scanner::threshhold(&mut image, options.threshold);
+    scanner::discard_blocks(&mut image, options.block_size as i32, options.block_fill_percent);
+    raster::save(&image, output.to_str().unwrap()).unwrap();
+}
+
+fn main() -> std::io::Result<()> {
     let options = Options::from_args();
     if !options.input.exists() {
         println!("Input {:?} does not exists", options.input);
@@ -28,25 +40,45 @@ fn main() {
 
     if options.input.is_dir() {
         if options.output.exists() && options.output.is_file() {
-            println!("Output exists and is a file, please provide non existing path or an existing directory.");
+            println!("Output exists and is a file, please provide a non existing path or an existing directory.");
             std::process::exit(1);
+        } else if !options.output.exists() {
+            std::fs::create_dir_all(options.output.clone())?;
+        }
+    } else if options.input.is_file() {
+        if options.output.exists() {
+            std::io::stdout()
+                .write(b"Output file already exists, overwrite it [y/N]?")
+                .unwrap();
+            std::io::stdout().flush().unwrap();
+            let mut answer = String::new();
+            std::io::stdin().lock().read_line(&mut answer)?;
+            if answer.trim() != "y" {
+                std::process::exit(0);
+            }
         }
     }
 
-    let files = if options.input.is_file() {
-        vec![options.input]
+    if options.input.is_file() {
+        handle_file(&options.input, &options.output, &options);
+    //        let mut image = raster::open(options.input.to_str().unwrap()).unwrap();
+    //        raster::filter::grayscale(&mut image).unwrap();
+    //        scanner::threshhold(&mut image, options.threshold);
+    //        scanner::discard_blocks(&mut image, options.block_size as i32, options.block_fill_percent);
+    //        raster::save(&image, options.output.to_str().unwrap()).unwrap();
     } else {
-        std::fs::canonicalize(&options.input)
-            .unwrap()
-            .read_dir()
-            .unwrap()
+        let files = std::fs::canonicalize(&options.input)?
+            .read_dir()?
             .map(|e| e.unwrap().path())
-            .collect::<Vec<_>>()
-    };
-
-    let mut image = raster::open("/home/kostrzewa/Pictures/page.jpg").unwrap();
-    raster::filter::grayscale(&mut image).unwrap();
-    scanner::threshhold(&mut image, options.threshold);
-    scanner::discard_blocks(&mut image, options.block_size as i32, options.block_fill_percent);
-    raster::save(&image, "/tmp/page_bw.jpg").unwrap();
+            .collect::<Vec<_>>();
+        for file in &files {
+            handle_file(file, &options.output.join(file.file_name().unwrap()), &options);
+            //            let mut image = raster::open(file.to_str().unwrap()).unwrap();
+            //            raster::filter::grayscale(&mut image).unwrap();
+            //            scanner::threshhold(&mut image, options.threshold);
+            //            scanner::discard_blocks(&mut image, options.block_size as i32, options.block_fill_percent);
+            //            raster::save(&image).unwrap();
+        }
+    }
+    Ok(())
 }
